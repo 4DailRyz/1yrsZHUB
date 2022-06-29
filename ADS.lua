078358bad4363d22bfe0441fe142c94d2fb996cc
4DailRyz <105856697+4DailRyz@users.noreply.github.com>
2022-06-29 10:36:25 +0700

    Create ADS.lua

---
+++ ADS.lua
@@ -0,0 +1,4 @@
+pcall(function()
+repeat until wait() game:IsLoaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/4DailRyz/1yrsZHUB/main/Anime%20Dimensions%20Simulator.lua"))()
+end)
