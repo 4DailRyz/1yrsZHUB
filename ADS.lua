cac43ffe64c4f591ba54eb49ce2e5b73aaea7db1
4DailRyz <105856697+4DailRyz@users.noreply.github.com>
2022-06-29 17:13:55 +0700

    Update ADS.lua

--- ADS.lua
+++ ADS.lua
@@ -1,2 +1,3 @@
+wait(5)--sometimes put in autoexec and it doesn't run this is why i put wait()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/4DailRyz/1yrsZHUB/main/Anime%20Dimensions%20Simulator.lua"))()
 
